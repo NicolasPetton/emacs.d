e083db6b4d68cf786b0c419f6edf38d5b7c08804
Nicolas Petton <nicolas@petton.fr>
2018-03-12 19:00:24 +0100

    Add nov drone

--- init.el
+++ init.el
@@ -466,6 +466,45 @@ be global."
                   `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
             (setq create-lockfiles nil)))
 
+(use-package nov
+  :mode ("\\.epub\\'" . nov-mode)
+  :config (progn
+            (defun my-nov-font-setup ()
+              (face-remap-add-relative 'variable-pitch
+                                       :family "Gentium Book Basic"
+                                       :height 1.5))
+            (add-hook 'nov-mode-hook 'my-nov-font-setup)
+
+            (require 'justify-kp)
+
+            (setq nov-text-width 62)
+
+            (defun my-nov-window-configuration-change-hook ()
+              (my-nov-post-html-render-hook)
+              (remove-hook 'window-configuration-change-hook
+                           'my-nov-window-configuration-change-hook
+                           t))
+
+            (defun my-nov-post-html-render-hook ()
+              (if (get-buffer-window)
+                  (let ((max-width (pj-line-width))
+                        buffer-read-only)
+                    (save-excursion
+                      (goto-char (point-min))
+                      (while (not (eobp))
+                        (when (not (looking-at "^[[:space:]]*$"))
+                          (goto-char (line-end-position))
+                          (when (> (shr-pixel-column) max-width)
+                            (goto-char (line-beginning-position))
+                            (pj-justify)))
+                        (forward-line 1))))
+
+                (add-hook 'window-configuration-change-hook
+                          'my-nov-window-configuration-change-hook
+                          nil t)))
+
+            (add-hook 'nov-post-html-render-hook 'my-nov-post-html-render-hook)))
+
 (use-package omnisharp
   :after csharp-mode
   :bind (:map omnisharp-mode-map
