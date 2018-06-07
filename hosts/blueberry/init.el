25c8430710fff696553d053e616e5c66dcba8ead
Nicolas Petton <nicolas@petton.fr>
2018-06-07 13:33:55 +0200

    Update ledger transaction import using boobank

--- hosts/blueberry/init.el
+++ hosts/blueberry/init.el
@@ -33,45 +33,36 @@
   :demand t)
 
 (use-package ledger
-  :init
-  (defvar nico-ledger-imports nil)
+  :init (progn
+          (add-hook 'ledger-mode-hook #'company-mode)
 
-  (defun nico-ledger-autosync ()
-    (interactive)
-    (switch-to-buffer (get-buffer-create "*ledger sync*"))
-    (erase-buffer)
-    (ledger-mode)
-    (shell-command "ledger-autosync -l ~/org/reference/ledger/ledger.dat ~/Downloads/\\ telechargement.ofx -a Assets:Checking:CE --fid 10" (current-buffer)))
+          (defvar boobank-ledger-accounts nil
+            "Alist of Boobank-account-id -> Ledger account used when importing from Boobank.")
+          (defvar boobank-ledger-file nil
+            "Path to the ledger file.")
+          (defvar boobank-ledger-import-since "2018-05-30")
 
-  (defun nico-ledger-convert-transaction ()
-    (interactive)
-    (save-excursion
-      (back-to-indentation)
-      (let* ((match (map-some (lambda (k v)
-				(when (search-forward k (line-end-position) t)
-				  k))
-			      nico-ledger-imports))
-	     (replacement (if match
-			      (map-elt nico-ledger-imports match)
-			    (read-string "Import as: " "Expenses:Misc"))))
-	(search-forward "Expenses:Misc")
-	(delete-backward-char 13)
-	(insert replacement)
-	(ledger-post-align-xact (point)))
-      (goto-char (point-min))
-      (while (re-search-forward "\\bEUR\\b" nil t)
-	(replace-match "€" nil t)))
-    (ledger-navigate-next-xact-or-directive))
-
-  (defun nico-ledger-convert-all-transactions ()
-    (interactive)
-    (while (not (eobp))
-      (nico-ledger-convert-transaction)))
-
-  :bind ((:map ledger-mode-map
-	       ("C-c M-i" . nico-ledger-convert-transaction)))
-  :config
-  (add-hook 'ledger-mode-hook #'company-mode))
+          (defun boobank-ledger-import ()
+            "Import transactions from boobank in Ledger format using \"ledger-autosync\"."
+            (interactive)
+            (let ((fid "10"))
+              (switch-to-buffer (get-buffer-create "*ledger sync*"))
+              (erase-buffer)
+              (ledger-mode)
+              (dolist (acc boobank-ledger-accounts)
+                (goto-char (point-max))
+                (message "Fetching transactions for account %s" (car acc))
+                (shell-command (format "boobank -f ofx history %s %s > /tmp/%s"
+                                       (car acc)
+                                       boobank-ledger-import-since
+                                       (car acc)))
+                (message "Converting transactions for account %s" (car acc))
+                (shell-command (format "ledger-autosync -l %s /tmp/%s -a %s --fid %s"
+                                       boobank-ledger-file
+                                       (car acc)
+                                       (cdr acc)
+                                       fid)
+                               t))))))
 
 (use-package prodigy
   :config
